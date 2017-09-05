04c8c50e9df582584ff89014a009c09cdf712c20
Andre Wachsmuth <awa@xima.de>
2017-09-05 09:42:20 +0200

    fixed imports

--- jqueryparser/src/main/java/com/github/blutorange/jqueryparser/xfc/EXfcCondition.java
+++ jqueryparser/src/main/java/com/github/blutorange/jqueryparser/xfc/EXfcCondition.java
@@ -5,36 +5,38 @@ import org.eclipse.jdt.annotation.NonNull;
 import com.github.blutorange.jqueryparser.ICondition;
 import com.github.blutorange.jqueryparser.NonNullBiFunction;
 
+import de.xima.cmn.criteria.FilterCriterion;
+
 /**
  * !l = (l||!l)&&!l
  */
 enum EXfcCondition implements ICondition<FilterCriterion, XfcContext> {
-	AND((l,r) -> l.and(r)),
-	OR((l,r) -> l.or(r)),
-	NOR((l,r) -> l.orNot(l).andNot(l).andNot(r)),
-	NAND((l,r) -> l.orNot(l).andNot(l).orNot(r)),
-	LEFT_IDENTITY((l,r) -> l),
-	RIGHT_IDENTITY((l,r) -> r),
-	LEFT_NEGATION((l,r) -> l.orNot(l).andNot(l)),
-	RIGHT_NEGATION((l,r) -> r.orNot(r).andNot(r)),
-	IMPLIES((l,r) -> l.orNot(r)),
-	NOT_IMPLIES((l,r) -> r.andNot(l)),
-	REVERSE_IMPLIES((l,r) -> r.orNot(l)),
-	REVERSE_NOT_IMPLIES((l,r) -> l.andNot(r)),
-	NEVER((l,r) -> l.andNot(l)),
-	ALWAYS((l,r) -> l.orNot(l)),
-	XOR((l,r) -> l.andNot(r).or(r.andNot(l))),
-	NXOR((l,r) -> l.and(r).orNot(l).and(l.and(r).orNot(r))),
-	;
+  AND((l, r) -> l.and(r)),
+  OR((l, r) -> l.or(r)),
+  NOR((l, r) -> l.orNot(l).andNot(l).andNot(r)),
+  NAND((l, r) -> l.orNot(l).andNot(l).orNot(r)),
+  LEFT_IDENTITY((l, r) -> l),
+  RIGHT_IDENTITY((l, r) -> r),
+  LEFT_NEGATION((l, r) -> l.orNot(l).andNot(l)),
+  RIGHT_NEGATION((l, r) -> r.orNot(r).andNot(r)),
+  IMPLIES((l, r) -> l.orNot(r)),
+  NOT_IMPLIES((l, r) -> r.andNot(l)),
+  REVERSE_IMPLIES((l, r) -> r.orNot(l)),
+  REVERSE_NOT_IMPLIES((l, r) -> l.andNot(r)),
+  NEVER((l, r) -> l.andNot(l)),
+  ALWAYS((l, r) -> l.orNot(l)),
+  XOR((l, r) -> l.andNot(r).or(r.andNot(l))),
+  NXOR((l, r) -> l.and(r).orNot(l).and(l.and(r).orNot(r))),
+  ;
 
-	private NonNullBiFunction<@NonNull FilterCriterion, @NonNull FilterCriterion, @NonNull FilterCriterion> function;
+  private NonNullBiFunction<@NonNull FilterCriterion, @NonNull FilterCriterion, @NonNull FilterCriterion> function;
 
-	private EXfcCondition(final NonNullBiFunction<FilterCriterion, FilterCriterion, FilterCriterion> function) {
-		this.function = function;
-	}
+  private EXfcCondition(final NonNullBiFunction<FilterCriterion, FilterCriterion, FilterCriterion> function) {
+    this.function = function;
+  }
 
-	@Override
-	public FilterCriterion reduce(final XfcContext context, final FilterCriterion left, final FilterCriterion right) {
-		return function.apply(left, right);
-	}
+  @Override
+  public FilterCriterion reduce(final XfcContext context, final FilterCriterion left, final FilterCriterion right) {
+    return function.apply(left, right);
+  }
 }
--- jqueryparser/src/main/java/com/github/blutorange/jqueryparser/xfc/EXfcStringOperator.java
+++ jqueryparser/src/main/java/com/github/blutorange/jqueryparser/xfc/EXfcStringOperator.java
@@ -9,66 +9,67 @@ import com.github.blutorange.jqueryparser.IOperator;
 import com.github.blutorange.jqueryparser.IOperatorNameProviding;
 import com.github.blutorange.jqueryparser.QueryBuilderEvaluatorException;
 import com.github.blutorange.jqueryparser.QueryBuilderEvaluatorException.Codes;
-import com.querydsl.core.types.Operator;
+
+import de.xima.cmn.criteria.FilterCriterion;
+import de.xima.cmn.criteria.FilterCriterion.Operator;
+import de.xima.cmn.criteria.FilterCriterion.StringOperator;
 
 enum EXfcStringOperator implements IOperator<FilterCriterion, XfcContext, String>, IOperatorNameProviding {
-	IS_NULL(0, (field, vals) -> new FilterCriterion(field, Operator.IS_NULL)),
-	IS_NOT_NULL(0, (field, vals) -> new FilterCriterion(field, Operator.IS_NULL)),
-	IS_EMPTY(
-			0,
-			(field, vals) -> new FilterCriterion(field, Operator.IS_NULL)
-					.or(new FilterCriterion(field, StringUtils.EMPTY, Operator.EQUAL))),
-	IS_NOT_EMPTY(
-			0,
-			(field, vals) -> new FilterCriterion(field, Operator.NOT_NULL)
-					.andNot(new FilterCriterion(field, StringUtils.EMPTY, Operator.EQUAL))),
-	EQUAL(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.EQUAL)),
-	NOT_EQUAL(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_EQUAL)),
-	IN(0, Integer.MAX_VALUE, (field, vals) -> new FilterCriterion(field, vals, Operator.IN)),
-	NOT_IN(0, Integer.MAX_VALUE, (field, vals) -> new FilterCriterion(field, vals, Operator.NOT_IN)),
-	CONTAINS(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.LIKE, StringOperator.EQUAL_LIKE_BOTH)),
-	BEGINS_WITH(
-			1,
-			(field, vals) -> new FilterCriterion(field, vals[0], Operator.LIKE, StringOperator.EQUAL_LIKE_BEFORE)),
-	ENDS_WITH(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.LIKE, StringOperator.EQUAL_LIKE_AFTER)),
-	NOT_CONTAINS(
-			1,
-			(field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_LIKE, StringOperator.EQUAL_LIKE_BOTH)),
-	NOT_BEGINS_WITH(
-			1,
-			(field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_LIKE, StringOperator.EQUAL_LIKE_BEFORE)),
-	NOT_ENDS_WITH(
-			1,
-			(field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_LIKE, StringOperator.EQUAL_LIKE_AFTER)),;
+  IS_NULL(0, (field, vals) -> new FilterCriterion(field, Operator.IS_NULL)),
+  IS_NOT_NULL(0, (field, vals) -> new FilterCriterion(field, Operator.IS_NULL)),
+  IS_EMPTY(
+      0,
+      (field, vals) -> new FilterCriterion(field, Operator.IS_NULL).or(new FilterCriterion(field, StringUtils.EMPTY, Operator.EQUAL))),
+  IS_NOT_EMPTY(
+      0,
+      (field, vals) -> new FilterCriterion(field, Operator.NOT_NULL).andNot(new FilterCriterion(field, StringUtils.EMPTY, Operator.EQUAL))),
+  EQUAL(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.EQUAL)),
+  NOT_EQUAL(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_EQUAL)),
+  IN(0, Integer.MAX_VALUE, (field, vals) -> new FilterCriterion(field, vals, Operator.IN)),
+  NOT_IN(0, Integer.MAX_VALUE, (field, vals) -> new FilterCriterion(field, vals, Operator.NOT_IN)),
+  CONTAINS(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.LIKE, StringOperator.EQUAL_LIKE_BOTH)),
+  BEGINS_WITH(
+      1,
+      (field, vals) -> new FilterCriterion(field, vals[0], Operator.LIKE, StringOperator.EQUAL_LIKE_BEFORE)),
+  ENDS_WITH(1, (field, vals) -> new FilterCriterion(field, vals[0], Operator.LIKE, StringOperator.EQUAL_LIKE_AFTER)),
+  NOT_CONTAINS(
+      1,
+      (field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_LIKE, StringOperator.EQUAL_LIKE_BOTH)),
+  NOT_BEGINS_WITH(
+      1,
+      (field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_LIKE, StringOperator.EQUAL_LIKE_BEFORE)),
+  NOT_ENDS_WITH(
+      1,
+      (field, vals) -> new FilterCriterion(field, vals[0], Operator.NOT_LIKE, StringOperator.EQUAL_LIKE_AFTER)),;
 
-	private int min;
-	private int max;
-	private Mapper mapper;
+  private int min;
+  private int max;
+  private Mapper mapper;
 
-	private EXfcStringOperator(final int exactly, final Mapper mapper) {
-		this(exactly, exactly, mapper);
-	}
+  private EXfcStringOperator(final int exactly, final Mapper mapper) {
+    this(exactly, exactly, mapper);
+  }
 
-	private EXfcStringOperator(final int min, final int max, final Mapper mapper) {
-		this.min = min;
-		this.max = max;
-		this.mapper = mapper;
-	}
+  private EXfcStringOperator(final int min, final int max, final Mapper mapper) {
+    this.min = min;
+    this.max = max;
+    this.mapper = mapper;
+  }
 
-	@Override
-	public FilterCriterion operate(final String field, @NonNull final String[] values)
-			throws QueryBuilderEvaluatorException {
-		if (values.length < min || values.length > max)
-			throw new QueryBuilderEvaluatorException(Codes.ILLEGAL_NUMBER_OF_VALUES, String.valueOf(values.length));
-		return mapper.map(field, values);
-	}
+  @Override
+  public FilterCriterion operate(final XfcContext context, final String field, @NonNull final String[] values)
+      throws QueryBuilderEvaluatorException {
+    if (values.length < min || values.length > max)
+      throw new QueryBuilderEvaluatorException(Codes.ILLEGAL_NUMBER_OF_VALUES, String.valueOf(values.length));
+    return mapper.map(field, values);
+  }
 
-	@Override
-	public String getOperatorName() {
-		return name().toLowerCase(Locale.ROOT);
-	}
+  @Override
+  public String getOperatorName() {
+    return name().toLowerCase(Locale.ROOT);
+  }
 
-	private interface Mapper {
-		FilterCriterion map(String field, String[] values) throws QueryBuilderEvaluatorException;
-	}
+  private interface Mapper {
+    FilterCriterion map(String field, String[] values) throws QueryBuilderEvaluatorException;
+  }
 }
